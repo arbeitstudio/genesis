09b2bfbfd748e2088b09b5349e07db5f3ca39ef4
Ferit Yiğit BALABAN <fybalaban@fybx.dev>
2023-04-30 16:00:28 +0300

    + sh: copy_over.sh

---
+++ staging/copy_over.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+#     Anticverse Webraider © 2023
+#     The Genesis Framework
+#     https://github.com/anticverse/genesis.git (anticverse/genesis)
+#     Maintainer: Ferit Yiğit BALABAN <balaban.ferityigit@anticverse.com>
+#
+#     genesis/staging/copy_over.sh
+
+mkdir -p "../../assets/js"
+cp -v ../js/*.js "../../assets/js/"
