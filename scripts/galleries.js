4fd95e5867f5535a96d91360da541547ffab6362
Ferit Yiğit BALABAN <fyb@fybx.dev>
2024-01-22 02:33:50 +0300

    basic script template
    Signed-off-by: Ferit Yiğit BALABAN <fyb@fybx.dev>

---
+++ scripts/galleries.js
@@ -0,0 +1,11 @@
+/*
+ *      Arbeit Studio © 2024
+ *      The Genesis Framework
+ *      https://github.com/arbeitstudio/genesis.git (arbeitstudio/genesis)
+ *      Maintainer: Ferit Yiğit BALABAN <fyb@fybx.dev>
+ *
+ *      genesis/scripts/galleries.js
+ */
+const galleryClasses = ["shiny"];
+
+var genesis = typeof genesis !== 'undefined' ? genesis : (genesis = 313, console.log("Close the world, .txen eht nepO :: genesis by arbeit studio"), genesis);
