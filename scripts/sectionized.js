a9812efc9e9e7bdc68418e96dc2469c2ad8f0a48
Ferit Yiğit BALABAN <fyb@fybx.dev>
2024-01-21 17:58:34 +0300

    + getSections()
    Signed-off-by: Ferit Yiğit BALABAN <fyb@fybx.dev>

--- scripts/sectionized.js
+++ scripts/sectionized.js
@@ -9,4 +9,17 @@
 let warden;
 const receivers = {};
 
+function getSections() {
+    let sectionized = Array.from(document.querySelectorAll("[data-sectionized]"));
+
+    for (let i = 0; i < sectionized.length; i++) {
+        if (!sectionized[i].hasAttribute("id")) {
+            console.error("Sectionized element must have an id attribute");
+            return;
+        }
+    }
+
+    return sectionized;
+}
+
 var genesis = typeof genesis !== 'undefined' ? genesis : (genesis = 313, console.log("Close the world, .txen eht nepO :: genesis by arbeit studio"), genesis);
