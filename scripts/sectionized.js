e89c458d67638f32609cca6aaf0629d7fe204d04
Ferit Yiğit BALABAN <fyb@fybx.dev>
2024-01-21 17:58:10 +0300

    basic layout
    Signed-off-by: Ferit Yiğit BALABAN <fyb@fybx.dev>

---
+++ scripts/sectionized.js
@@ -0,0 +1,12 @@
+/*
+ *      Arbeit Studio © 2024
+ *      The Genesis Framework
+ *      https://github.com/arbeitstudio/genesis.git (arbeitstudio/genesis)
+ *      Maintainer: Ferit Yiğit BALABAN <fyb@fybx.dev>
+ *
+ *      genesis/scripts/sectionized.js
+ */
+let warden;
+const receivers = {};
+
+var genesis = typeof genesis !== 'undefined' ? genesis : (genesis = 313, console.log("Close the world, .txen eht nepO :: genesis by arbeit studio"), genesis);
