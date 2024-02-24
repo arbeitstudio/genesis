c2a161f9661609a62dbfc497ac3efae8e443f5a5
Ferit Yiğit BALABAN <fyb@fybx.dev>
2024-02-24 15:24:37 +0300

    Add blobs.js file for Genesis framework
    Signed-off-by: Ferit Yiğit BALABAN <fyb@fybx.dev>

---
+++ scripts/blobs.js
@@ -0,0 +1,11 @@
+/*
+ *      Arbeit Studio © 2024
+ *      The Genesis Framework
+ *      https://github.com/arbeitstudio/genesis.git (arbeitstudio/genesis)
+ *      Maintainer: Ferit Yiğit BALABAN <fyb@fybx.dev>
+ *
+ *      genesis/scripts/blobs.js
+ */
+let blobPositionAlgorithm;
+let blobContainerTuples = [];
+const body = document.body;
